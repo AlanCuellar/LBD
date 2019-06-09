26a8795b41e436ed7ff99e5b1b982baefdd54f0f
AlanCuellar <47695938+AlanCuellar@users.noreply.github.com>
2019-06-08 21:39:04 -0500

    Add files via upload

---
+++ Practica7lbd.sql
@@ -0,0 +1,39 @@
+create view Soldado as vista_soldado
+select id_cod, cod_prod, nombre from Soldado
+
+select * from Soldado
+
+
+create view Servicio as vista_Servicio
+select id_, cod_prod, nombre from Servicios	
+
+select * from Servicio
+
+
+create view Cuartel as vista_cuartel
+select id_cod, cod_prod, nombre from Cuartel
+
+select * from Cuartel
+
+
+create view Servicios as vista_Servicios
+select id_cod, cod_prod, nombre from Servicios
+
+select * from Servicios
+
+
+create view Compañia as vista_compañia
+select id_cod, cod_prod, nombre from Compañia
+
+select * from Compañia
+
+
+create view Cuerpo as vista_cuerpo
+select id_cod, cod_prod, nombre from Cuerpo
+
+select * from Cuerpo
+
+create view Grado as vista_grado
+select id_cod, cod_prod, nombre from Grado
+
+select * from Grado
